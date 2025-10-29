631ab1e7f37809548c7228f5394ba146c4f801ec
DucIsHere <thelegendwolf07@gmail.com>
2025-10-29 13:02:53 +0700

    Update aurora_the_end.glsl

--- shaderpacks/Fantasy/shaders/include/aurora_the_end.glsl
+++ shaderpacks/Fantasy/shaders/include/aurora_the_end.glsl
@@ -1,3 +1,8 @@
 vec3 auroraColor = vec3(0.35, 0.0, 0.6);
 auroraColor *= 1.5 * smoothstep(0.6, 1.0, noiseVal);
 float n = fbm(uv * scale + vec2(time * 0.04, 0.0));
+float n2 = fbm(uv * scale * 0.4 - vec2(time * 0.015, 0.0));
+float auroraWave = mix(n, n2, 0.5);
+float flicker = 0.9 + 0.1 * sin(time * 0.8); // Flicker Smooth
+color *= flicker;
+color *= smoothstep(0.0, 0.6, 1.0 - abs(uv.y)); // Fade Horizons
