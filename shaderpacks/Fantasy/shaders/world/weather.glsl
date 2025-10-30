a4322db6d72b8a11fb1996d3fca216ab040ad7dc
DucIsHere <thelegendwolf07@gmail.com>
2025-10-30 18:12:45 +0700

    Update and rename shaderpacks/Fantasy/shaders/weather.glsl to shaderpacks/Fantasy/shaders/world/weather.glsl

--- shaderpacks/Fantasy/shaders/world/weather.glsl
+++ shaderpacks/Fantasy/shaders/world/weather.glsl
@@ -1,3 +1,5 @@
 float fogFactor = exp(-distance * 0.02);
 vec3 fogColor = mix(skyColor, auroraColor, 0.2);
 fragColor.rgb = mix(fogColor, fragColor.rgb, fogFactor);
+float dust = texture(noiseTex, vec2(worldPos.xz * 0.002 + time * 0.02)).r;
+fragColor.rgb += vec3(0.2, 0.1, 0.15) * dust * 0.1;
