118670309cd8caf71f0ed6337d02a95baa61c64d
DucIsHere <thelegendwolf07@gmail.com>
2025-10-30 18:21:17 +0700

    Delete shaderpacks/Fantasy/shaders/world1/terrian.glsl

--- shaderpacks/Fantasy/shaders/world1/terrian.glsl
+++
@@ -1,4 +0,0 @@
-float light = max(dot(normal, sunDir), 0.0);
-vec3 ambient = vec3(0.2, 0.05, 0.1);
-vec3 tint = vec3(0.8, 0.3, 0.5) * 0.2; // ánh đỏ tím môi trường
-fragColor.rgb = texture(blockTex, uv).rgb * (ambient + light) + tint;
