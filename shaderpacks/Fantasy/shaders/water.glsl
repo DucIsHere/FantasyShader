6078a063fe69f41b7f507a09e2e03b939020c1f2
DucIsHere <thelegendwolf07@gmail.com>
2025-11-02 11:30:52 +0700

    Delete shaderpacks/Fantasy/shaders/water.glsl

--- shaderpacks/Fantasy/shaders/water.glsl
+++
@@ -1,9 +0,0 @@
-vec3 refl = texture(reflectTex, reflectCoord).rgb;
-vec3 refr = texture(refractTex, refractCoord).rgb;
-
-// Ánh sáng aurora và trăng phản xuống
-vec3 auroraRefl = auroraColor * 0.5;
-vec3 moonRefl   = moonColor * 0.3;
-
-// Combine
-fragColor.rgb = mix(refr, refl, fresnel) + auroraRefl + moonRefl;
